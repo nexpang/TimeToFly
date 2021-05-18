527923f08613bd20697b12ba92b479e8f08da0a4
Double_U_ <junhyup511@naver.com>
2021-05-18 23:49:57 +0900

    수정 완료!

--- Assets/01. Scripts/InGame/GameManager.cs
+++ Assets/01. Scripts/InGame/GameManager.cs
@@ -16,6 +16,7 @@ public class GameManager : MonoBehaviour
 
     private void Awake()
     {
+        Time.timeScale = 1;
         Instance = this; // ΩÃ±€≈Ê
         timer = defaultTimer; // ≈∏¿Ã∏” √ ±‚»≠
     }
