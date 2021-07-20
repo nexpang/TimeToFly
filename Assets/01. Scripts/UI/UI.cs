51a045bc3a8ad3a9564ad036563620adf387559e
Double_U_ <junhyup511@naver.com>
2021-07-20 23:24:36 +0900

    폴더 정리, 타이틀 개선

--- Assets/01. Scripts/UI/UI.cs
+++ Assets/01. Scripts/UI/UI.cs
@@ -8,6 +8,13 @@ public class UI : MonoBehaviour
     [SerializeField] 
     private Text startTxt = null;
     [SerializeField]
+    private Transform chickenTranform = null;
+    [SerializeField]
+    private Transform cloud = null;
+    [SerializeField]
+    private Transform logo = null;
+
+    [SerializeField]
     private GameObject exit = null;
     [SerializeField]
     private GameObject clearPanel = null;
@@ -25,7 +32,15 @@ public class UI : MonoBehaviour
 
     void Start()
     {
-        startTxt.DOColor(new Color(1f, 1f, 1f, 10f), 0.8f).SetLoops(-1, LoopType.Yoyo);
+        startTxt.DOColor(new Color(0f, 0f, 0f, 10f), 0.8f).SetLoops(-1, LoopType.Yoyo);
+        startTxt.GetComponent<Outline>().DOColor(new Color(1f, 1f, 1f, 10f), 0.8f).SetLoops(-1, LoopType.Yoyo);
+
+        chickenTranform.DOScaleY(0.8f, 1.75f).SetEase(Ease.InOutSine).SetLoops(-1, LoopType.Yoyo);
+        chickenTranform.DOScaleX(0.7f, 1.5f).SetEase(Ease.InOutSine).SetLoops(-1, LoopType.Yoyo);
+
+        cloud.DOMoveY(0.3f,2).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine).SetRelative();
+
+        logo.DOScale(1.1f,2).SetEase(Ease.InOutSine).SetLoops(-1, LoopType.Yoyo);
     }
     private void Update()
     {
