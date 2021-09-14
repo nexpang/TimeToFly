e999282566d08264cf0d3bf493daddb0032ca914
nexpang <rlarudgur123@gmail.com>
2021-09-14 20:19:31 +0900

    튜토 ㅈ버그고침

--- Assets/01. Scripts/InGame/Objects/Tutorial/ChickenObject.cs
+++ Assets/01. Scripts/InGame/Objects/Tutorial/ChickenObject.cs
@@ -20,6 +20,18 @@ public class ChickenObject : InteractionObject
     {
         if(collision.CompareTag("Player"))
         {
+            Ability_FutureCreate ability1 = (Ability_FutureCreate)GameManager.Instance.player.abilitys[(int)Chickens.BROWN];
+
+            Ability_TimeFaster ability2 = (Ability_TimeFaster)GameManager.Instance.player.abilitys[(int)Chickens.BLUE];
+            if (ability1.enabled && ability1.isAbilityEnable) // 만약 능력 1이고 자고있는 상태면
+            {
+                ability1.ResetPlayer();
+                return;
+            }
+            else if(ability2.enabled && ability2.isAbilityEnable)
+            {
+                ability2.ResetPlayer();
+            }
             isAlreadyChange = false;
             GameManager.Instance.player._speed = 0;
             collider2D.enabled = false;
